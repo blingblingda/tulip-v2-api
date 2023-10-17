aa5332f715c510287f8bf09a65916c8c37eadbb0
YuJi-2023 <sophia.jiyu@gmail.com>
2023-10-17 12:20:59 +1100

    build connection to MongoDB and update package.json

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require("express");
 
+const mongoose = require("mongoose");
+
 const routes = require("./routes/index");
+const e = require("express");
 
-const tulip = express();
-const port = 1245;
+const app = express();
+const port = 3000;
 
-tulip.use("/", routes);
+app.use("/", routes);
 
-tulip.listen(port, () => {
-  console.log(`Listening on port${port}`);
-});
-
-module.exports = tulip;
+mongoose
+  .connect(
+    "REMOVED_SENSITIVE_VALUE"
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`app API is running on port: ${port}`);
+    });
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+module.exports = app;
